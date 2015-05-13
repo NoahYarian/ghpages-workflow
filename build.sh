9ad122309310fc0527c2a2c61a1ec734be1db56a
Scott Humphries <scott@sscotth.io>
2015-05-13 15:10:45 -0500

    add guard to bowcat incase there are no bower_components

--- build.sh
+++ build.sh
@@ -24,7 +24,9 @@ rm -rf src/*.html \
 ./node_modules/.bin/babel src --out-dir public -s inline
 
 # concat bower_components to public/lib directory
-./node_modules/.bin/bowcat . -o public/lib -m
+if [ -d "bower_components" ]; then
+  ./node_modules/.bin/bowcat . -o public/lib -m
+fi
 
 # clean unneeded files
 rm -rf public/_styles \
